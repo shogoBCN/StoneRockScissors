240019c392e52084cd886bdc56e0fd4e53a1c9a8
shogoBCN <56768230+shogoBCN@users.noreply.github.com>
2022-02-15 21:39:53 +0100

    logic updates & styling

--- js/blackjack.js
+++ js/blackjack.js
@@ -4,6 +4,16 @@ var cardValues = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "
 var fullDeck = [];
 var player;
 var dealer;
+var money = 1000;
+var minBet = 100;
+var bet;
+var moneyDiv = document.getElementById("money");
+var betDiv = document.getElementById("bet");
+var gameResultDiv = document.getElementById("gameResult")
+var coinDisplayDiv = document.getElementById("coinDisplay")
+var cardsOnTable = 0;
+moneyDiv.innerHTML = money + "€";
+betDiv.value = minBet;
 
 // card deck creation
 function makeDeck() {
@@ -30,40 +40,66 @@ function shuffleDeck(array) {
   }
 }
 
-function makeCard(what, who) {
+// making cards
+function makeCard(what, who, which) {
+  cardsOnTable++;
   var currenthand = document.getElementById(who + "Cards");
-  currenthand.appendChild(renderCard(what));
+  currenthand.appendChild(renderCard(what, which));
+  
 }
-function renderCard(what) {
-  var cardDiv = document.createElement("div");
-  var cardIcon = "";
-  cardDiv.setAttribute("class", "card");
-    if (what.CardSuit == "club") {
-      cardIcon = "&clubs;";
+function renderCard(what, which) {
+  if (what == "dealerFirst") {
+    what = dealer.currentHand[0];
+    var cardDiv = document.getElementById("dealerFirst")
+  }  
+  else {
+    var cardDiv = document.createElement("div");
+    var cardIcon = "";
+    if (which === "init") {
+      cardDiv.setAttribute("class", "card " + what.CardSuit + " card" + cardsOnTable + " init");
+      cardDiv.setAttribute("id", "card"); 
     }
-    else if (what.CardSuit == "spade") {
-      cardIcon = '&spades;';
-    }
-    else if (what.CardSuit == "diamond") {
-      cardIcon = '&diams;';
-    } 
     else {
-      cardIcon = '&hearts;';
+      cardDiv.setAttribute("class", "card " + what.CardSuit + " card" + cardsOnTable);
+      cardDiv.setAttribute("id", "card"); 
+      cardDiv.style.animation = "fade-in 1s";
     }
-    cardDiv.innerHTML = what.CardValue + cardIcon;
-    return cardDiv;
+  }
+  if (what.CardSuit == "club") {
+    cardIcon = "&clubs;";
+  }
+  else if (what.CardSuit == "spade") {
+    cardIcon = '&spades;';
+  }
+  else if (what.CardSuit == "diamond") {
+    cardIcon = '&diams;';
+  } 
+  else {
+    cardIcon = '&hearts;';
+  }
+  cardDiv.innerHTML = 
+    '<span class="cardContent topLeft">' + what.CardValue + cardIcon + '</span>' + 
+    '<span class="cardSuit">' + cardIcon + '</span>' + 
+    '<span class="cardContent downRight">' + what.CardValue + cardIcon + '</span>';
+  return cardDiv;
 }
 
 // 2 cards to player & dealer each 
 // checking for blackjacks
 function initDeal() {
+  var init = "init";
   for (var i = 0; i < 2; i++) {
+    card = fullDeck.pop()
+    dealer.currentHand.push(card)
+    makeCard(card, dealer.Name, init)
+    if (i == 0) {
+      document.getElementById("card").setAttribute("id", "dealerFirst"); 
+      document.getElementById("dealerFirst").innerHTML = "  ";
+      document.getElementById("dealerFirst").setAttribute("style", "background-image: url(./img/cardbackground.png);");
+    }
     var card = fullDeck.pop();
     player.currentHand.push(card);
-    makeCard(card, player.Name)
-    card = fullDeck.pop();
-    dealer.currentHand.push(card);
-    makeCard(card, dealer.Name)
+    makeCard(card, player.Name, init)
   }
   sumPoints(player)
   sumPoints(dealer)
@@ -71,14 +107,14 @@ function initDeal() {
   document.getElementById("newGame").disabled = true;
   if (dealer.combinedWeight == 21) {
     if (player.combinedWeight == 21) {
-      endGame(false, "bothBJ")
+      setTimeout(() =>endGame(false, "bothBJ"), 1000)
     }
     else {
-      endGame(false, "dealerBJ")
+      setTimeout(() =>endGame(false, "dealerBJ"), 1000)
     }
   }
   else if (player.combinedWeight == 21) {
-    endGame(true, "playerBJ")
+    setTimeout(() =>endGame(true, "playerBJ"), 1000)
   }
 }
 
@@ -88,21 +124,26 @@ function sumPoints(who) {
   var total = 0;
   var ace = false;
   for (var i = 0; i < who.currentHand.length; i++) {
-    total += who.currentHand[i].CardWeight;
+    total += who.currentHand[i].CardWeight;//I LOVE MAGOCITA!
     if (who.currentHand[i].CardWeight == 1) {
       ace = true;
     }
   }
   if ( total + 10 <= 21 && ace) {
-    total +=10; 
+    total += 10; 
   }
   who.combinedWeight = total;
-  document.getElementById(who.Name + "Points").innerHTML = who.combinedWeight;
+  document.getElementById("playerPoints").innerHTML = player.combinedWeight;
 }
 
 // deletes visuals, resets objects, starts the game
 function newGame() {
-  document.getElementById("gameResult").innerHTML = "";
+
+  bet = betDiv.value;
+  document.getElementById("dealerPoints").innerHTML = "";
+  document.getElementById("playerPoints").innerHTML = "";
+  gameResultDiv.innerHTML = "";
+  document.getElementById("bet").disabled = true;
   document.getElementById("hitMe").disabled = false;
   document.getElementById("stand").disabled = false;
   var allCards = document.querySelectorAll(".card");
@@ -113,10 +154,20 @@ function newGame() {
   player = { Name: "player", combinedWeight: 0, currentHand: [] };
   dealer = { Name: "dealer", combinedWeight: 0, currentHand: [] };
   fullDeck = [];
-  makeDeck()
-  shuffleDeck(fullDeck)
-  initDeal()
+  if (bet < minBet || bet > money) {
+    gameResultDiv.innerHTML = "Please choose a bet between 100€ and your current balance (" + money + "€)";
+    document.getElementById("hitMe").disabled = true;
+    document.getElementById("stand").disabled = true;
+    document.getElementById("bet").disabled = false;
   }
+  else {
+    money = money - bet;
+    moneyDiv.innerHTML = money + "€";
+    makeDeck()
+    shuffleDeck(fullDeck)
+    initDeal()
+  }
+}
 
 // hitme button
 function hitMe() {
@@ -125,54 +176,97 @@ function hitMe() {
   makeCard(card, player.Name)
   sumPoints(player)
   console.log(player,dealer);
-  pointsCheck()
-}
-
-function pointsCheck() {
-  if (player.combinedWeight > 21) {
-    endGame(false, "over21")
+  if (player.currentHand.length !== 5 && player.combinedWeight < 21) {
+  gameResultDiv.innerHTML = "You have " + player.combinedWeight + " points. Hit or Stand?"
+  }
+  else if (player.combinedWeight > 21) {
+    setTimeout(() =>endGame(false, "playerOver21"), 1000)
+  }
+  else if (player.currentHand.length == 5 && player.combinedWeight < 21) {
+    setTimeout(() =>endGame(true, "5cards"), 1000)
   }
 }
-
 
 function stand() {
+  setTimeout(() => renderCard("dealerFirst"), 1000);
   for (var i = 0; i < 5; i++) {
-    if (dealer.combinedWeight <= 16 && dealer.currentHand.length < 5) {
+  if (dealer.combinedWeight <= 16 && dealer.currentHand.length < 5) {
       var card = fullDeck.pop();
       dealer.currentHand.push(card);
+      makeCard(card, dealer.Name)
       sumPoints(dealer)
-      console.log(player,dealer);
-    }
-    else {
-      break;
-    }
-  }  
+      console.log(player,dealer)
+  }
+  else {
+    break;
+  }
+  } 
+  if (dealer.combinedWeight > 21) {
+    setTimeout(() =>endGame(true, "dealerOver21"), 1000)
+  }
+  else if (dealer.combinedWeight > player.combinedWeight) {
+    setTimeout(() =>endGame(false, "lessPoints"), 1000)
+  }
+  else if (dealer.combinedWeight < player.combinedWeight) {
+    setTimeout(() =>endGame(true, "morePoints"), 1000)
+  }
+  else if (dealer.combinedWeight == player.combinedWeight) {
+    setTimeout(() =>endGame(false, "tie"), 1000)
+  }
+  if (dealer.currentHand.length == 5 && dealer.combinedWeight < 21) {
+    setTimeout(() =>endGame(false, "5cards"), 1000)
+  }
 }
 
-
 function endGame(win, why) {
-  if (win == false && why == "over21") {
-    document.getElementById("gameResult").innerHTML = "So sorry, you're over 21 and YOU LOSE!"
+  cardsOnTable = 0;
+  renderCard("dealerFirst");
+  document.getElementById("dealerFirst").setAttribute("style", "background-color: white");
+  document.getElementById("dealerPoints").innerHTML = dealer.combinedWeight;
+  if (win == false && why == "tie") {
+    gameResultDiv.innerHTML = "Tie, nobody wins!"
+    money = money + (bet*1);
   }
-  else if (win == true && why == "over21") {
-    document.getElementById("gameResult").innerHTML = "YAY! Dealer is over 21! YOU WIN!"
+  else if (win == false && why == "playerOver21") {
+    gameResultDiv.innerHTML = "So sorry, you're over 21 and YOU LOSE!"
   }
-  else if (win == true && why == "lessPoints") {
-    document.getElementById("gameResult").innerHTML = "You have " + player.combinedWeight + " and Dealer has " + dealer.combinedWeight + ". YOU WIN!"
+  else if (win == true && why == "dealerOver21") {
+    gameResultDiv.innerHTML = "YAY! Dealer is over 21! YOU WIN!"
+    money = money + (bet*2); 
+  }
+  else if (win == true && why == "morePoints") {
+    gameResultDiv.innerHTML = "You have " + player.combinedWeight + " and Dealer has " + dealer.combinedWeight + ". YOU WIN!"
+    money = money + (bet*2); 
   }
   else if (win == false && why == "lessPoints") {
-    document.getElementById("gameResult").innerHTML = "You have " + player.combinedWeight + " and Dealer has " + dealer.combinedWeight + ". YOU LOSE!"
+    gameResultDiv.innerHTML = "You have " + player.combinedWeight + " and Dealer has " + dealer.combinedWeight + ". YOU LOSE!"
   }
   else if (win == false && why == "dealerBJ") {
-    document.getElementById("gameResult").innerHTML = "Dealer has Blackjack. YOU LOSE!"
+    gameResultDiv.innerHTML = "Dealer has Blackjack. YOU LOSE!"
   }
   else if (win == true && why == "playerBJ") {
-    document.getElementById("gameResult").innerHTML = "You have Blackjack. YOU WIN!"
+    gameResultDiv.innerHTML = "You have Blackjack. YOU WIN!"
+    money = money + (bet*2); 
   }
   else if (win == false && why == "bothBJ") {
-    document.getElementById("gameResult").innerHTML = "Both have Blackjack. YOU LOSE!"
+    gameResultDiv.innerHTML = "Both have Blackjack. YOU LOSE!"
+  }
+  else if (win == true && why == "5cards") {
+    gameResultDiv.innerHTML = "You have 5 cards and are still under 21. YOU WIN!"
+    money = money + (bet*2); 
+  }
+  else if (win == false && why == "5cards") {
+    gameResultDiv.innerHTML = "Dealer has 5 cards and is still under 21. YOU LOSE!"
   }
   document.getElementById("hitMe").disabled = true;
   document.getElementById("stand").disabled = true;
   document.getElementById("newGame").disabled = false;
+  document.getElementById("bet").disabled = false;
+  moneyDiv.innerHTML = money + "€";
+  if (money < 100) {
+    document.getElementById("bet").disabled = true;
+    document.getElementById("newGame").disabled = true;
+    coinDisplayDiv.innerHTML = "You're out of Coin. GAME OVER!"
+    betDiv.value = "GG!"
+  }
 }
